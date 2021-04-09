b747993bc95e175b7ebb68404313c4955f1bc1ae
dnn004 <dnn004@ucsd.edu>
2021-04-09 15:59:24 -0700

    Add utils

--- App.js
+++ App.js
@@ -2,13 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+
+  componentDidMount() {
+    console.log("PRINTING")
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hi up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +25,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
